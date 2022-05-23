ed6a202cd59f2e566b06fa29938cd4ac72a60822
FrizDev057 <61758522+FrizDev057@users.noreply.github.com>
2022-05-23 13:04:06 +0300

    Убраны бессмысленные переменные

--- VanillaMobs/src/VanillaMobs/entity/BaseEntity.php
+++ VanillaMobs/src/VanillaMobs/entity/BaseEntity.php
@@ -8,22 +8,20 @@ use pocketmine\nbt\tag\{CompoundTag, ListTag, DoubleTag, FloatTag};
 abstract class BaseEntity extends Creature{
 
 public function nbtShoot(){
-      $yaw = $this->yaw;
-     $pitch = $this->pitch;
             return new CompoundTag("", [
                 "Pos" => new ListTag("Pos", [
-                    new DoubleTag("", $this->x + (-sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5)),
+                    new DoubleTag("", $this->x + (-sin($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI) * 0.5)),
                     new DoubleTag("", $this->y + 1.62),
-                    new DoubleTag("", $this->z +(cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5))
+                    new DoubleTag("", $this->z +(cos($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI) * 0.5))
                 ]),
                 "Motion" => new ListTag("Motion", [
-                    new DoubleTag("", -sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2),
-                    new DoubleTag("", -sin($pitch / 180 * M_PI) * 1.2),
-                    new DoubleTag("", cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2)
+                    new DoubleTag("", -sin($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI) * 1.2),
+                    new DoubleTag("", -sin($this->pitch / 180 * M_PI) * 1.2),
+                    new DoubleTag("", cos($this->yaw / 180 * M_PI) * cos($this->pitch / 180 * M_PI) * 1.2)
                 ]),
                 "Rotation" => new ListTag("Rotation", [
-                    new FloatTag("", $yaw),
-                    new FloatTag("", $pitch)
+                    new FloatTag("", $this->yaw),
+                    new FloatTag("", $this->pitch)
                 ]),
             ]);
   }
