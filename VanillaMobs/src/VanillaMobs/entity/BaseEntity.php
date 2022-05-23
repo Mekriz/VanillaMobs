ef36e1d9935b20285ccc1afef97f817327d365fa
FrizDev057 <61758522+FrizDev057@users.noreply.github.com>
2022-05-23 12:33:22 +0300

    Добавление общей функции для выстрелов nbtShoot()

--- VanillaMobs/src/VanillaMobs/entity/BaseEntity.php
+++ VanillaMobs/src/VanillaMobs/entity/BaseEntity.php
@@ -6,4 +6,24 @@ use pocketmine\entity\Creature;
 
 abstract class BaseEntity extends Creature{
 
+public function nbtShoot(){
+      $yaw = $this->yaw;
+     $pitch = $this->pitch;
+            return new CompoundTag("", [
+                "Pos" => new ListTag("Pos", [
+                    new DoubleTag("", $this->x + (-sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5)),
+                    new DoubleTag("", $this->y + 1.62),
+                    new DoubleTag("", $this->z +(cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5))
+                ]),
+                "Motion" => new ListTag("Motion", [
+                    new DoubleTag("", -sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2),
+                    new DoubleTag("", -sin($pitch / 180 * M_PI) * 1.2),
+                    new DoubleTag("", cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2)
+                ]),
+                "Rotation" => new ListTag("Rotation", [
+                    new FloatTag("", $yaw),
+                    new FloatTag("", $pitch)
+                ]),
+            ]);
+  }
 }
