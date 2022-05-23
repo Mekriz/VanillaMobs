9cb8be2ffea7915fc5b8f2c17bdb8c12495f2987
FrizDev057 <61758522+FrizDev057@users.noreply.github.com>
2022-05-23 12:36:16 +0300

    Использование общей функции nbtShoot()
    Оптимизация кода

--- VanillaMobs/src/VanillaMobs/entity/monster/Skeleton.php
+++ VanillaMobs/src/VanillaMobs/entity/monster/Skeleton.php
@@ -66,26 +66,7 @@ public $dropExp = [1, 3];
     parent::processMove();
     $this->onSun();
 if($this->attackDelay > 60 and $this->isnear != null){
-            $yaw = $this->yaw;
-            $pitch = $this->pitch;
-            $nbt = new CompoundTag("", [
-                "Pos" => new ListTag("Pos", [
-                    new DoubleTag("", $this->x + (-sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5)),
-                    new DoubleTag("", $this->y + 1.62),
-                    new DoubleTag("", $this->z +(cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5))
-                ]),
-                "Motion" => new ListTag("Motion", [
-                    new DoubleTag("", -sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2),
-                    new DoubleTag("", -sin($pitch / 180 * M_PI) * 1.2),
-                    new DoubleTag("", cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 1.2)
-                ]),
-                "Rotation" => new ListTag("Rotation", [
-                    new FloatTag("", $yaw),
-                    new FloatTag("", $pitch)
-                ]),
-            ]);
-    								            $arrow = Entity::createEntity("Arrow", $this->level, $nbt, $this);
-
+            $arrow = Entity::createEntity("Arrow", $this->level, $this->nbtShoot(), $this);
             $ev = new EntityShootBowEvent($this, Item::get(Item::ARROW, 0, 1), $arrow, 1.2);
             $this->server->getPluginManager()->callEvent($ev);
       $this->attackDelay = 0;
